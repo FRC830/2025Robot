e90beb6284dfc0561b3dfd03381f88ed77cae180
JoeyBrar <jorabrar2007@gmail.com>
2025-01-05 15:57:13 -0500

    started on MoveToPose class

---
+++ src/main/cpp/MoveToPose.cpp
@@ -0,0 +1,14 @@
+#include <frc/geometry/Pose2d.h>
+#include <cmath>
+
+
+
+class MoveToPose
+{
+    private:
+    
+    public:
+        MoveToPose() = default;
+        ~MoveToPose() = default;
+         
+}
