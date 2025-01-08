d0260c7fda780211862041dfe64cc0480c1fc6a5
Harsh-Sinha <harsh.sinha@hotmail.com>
2025-01-07 22:19:21 -0500

    Fixed MoveToPose header and cpp files

--- src/main/cpp/MoveToPose.cpp
+++ src/main/cpp/MoveToPose.cpp
@@ -1,58 +1,91 @@
-// TODO: classes we define should have "" instead of <> for their include statement
-#include <MoveToPose.h>
+#include "MoveToPose.h"
 
 // TODO: look at these files to understand how to setup and use a trapezoid move using switch statements
-         - https://github.com/FRC830/2024Robot/blob/master/src/main/include/subsystems/IntakeHAL.h (trapezoid move member variable init)
-         - https://github.com/FRC830/2024Robot/blob/master/src/main/cpp/subsystems/IntakeHAL.cpp (ProfiledMoveToAngle function)
+        // - https://github.com/FRC830/2024Robot/blob/master/src/main/include/subsystems/IntakeHAL.h (trapezoid move member variable init)
+       //  - https://github.com/FRC830/2024Robot/blob/master/src/main/cpp/subsystems/IntakeHAL.cpp (ProfiledMoveToAngle function)
 // TODO: don't worry about doing anything with the drive base for now
 
 void MoveToPose::move(frc::Pose2d desired) {
-    // TODO: this should be in a switch statement:
-    //       - case 0: query swerve for current position and save to member variable
-    //       - case 1: call move(current, desired)
-     // TODO: query swerve drive for current pose
-     frc::Pose2d current = frc::Pose2d(0, 0, frc::Rotation2d(0));
-     move(current, desired);
-};
-
-void MoveToPose::move(frc::Pose2d desired, frc::Pose2d current) {
-    angularRotation(desired.getRotation(), current.getRotation());
-    linearTranslatinon(current, desired);   
-};
-
-double MoveToPose::angularRotation(frc::Pose2d desired, frc::Pose2d current) {
-    switch (m_MoveToState)
-    {
-    case 0:
-        m_timer.Restart();
-        // todo get the angle
-        m_MoveToState++;
-        break;
-    case 1:
-        m_Profile.Calculate(
-            m_timer.Get(),
-            frc::TrapezoidProfile<units::degrees>::State{desired, 0},
-            frc::TrapezoidProfile<units::degrees>::State{current, 0}           
-        );
-
-        // todo set a variable for line 35 and rotate the robot
-
-        if (m_Profile.IsFinished(m_timer.Get())) {
+    frc::Pose2d current;    
+    switch(m_MoveToState){
+        case 0:
+        {
+            // query swerve for current position when that function exists later
+            current = frc::Pose2d{0_m, 0_m, frc::Rotation2d(0_deg)};
             m_MoveToState++;
+            break;
         }
-
-        break;
+        case 1:
+        {
+            move(current, desired);
+            m_MoveToState++;
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
     
-    case 2:
-        m_timer.Stop();
-        m_MoveToState++;
-        break;
+};
 
-    default:
-        break;
+void MoveToPose::move(frc::Pose2d current, frc::Pose2d desired) {
+    angularRotation(current.Rotation(), desired.Rotation());
+    linearTranslation(current, desired);  
+};
+
+void MoveToPose::angularRotation(frc::Rotation2d current, frc::Rotation2d desired) {
+    switch (m_MoveAngleToState)
+    {
+        case 0:
+        {
+            m_timer.Restart();
+            
+            
+            m_MoveAngleToState++;
+            break;
+        }
+        case 1:
+        {
+            auto setPoint = m_Profile.Calculate(
+                m_timer.Get(),
+                frc::TrapezoidProfile<units::degrees>::State{current.Degrees(), 0_deg_per_s},
+                frc::TrapezoidProfile<units::degrees>::State{desired.Degrees(), 0_deg_per_s}           
+            );
+
+            // todo rotate robot with setpoint when that function exists later
+
+            if (m_Profile.IsFinished(m_timer.Get())) {
+                m_MoveAngleToState++;
+            }
+
+            break;
+        }
+        case 2:
+        {
+            m_timer.Stop();
+            m_MoveAngleToState++;
+            break;
+        }
+        default:
+        {
+            break;
+        }
     }
 };
 
-double linearTranslatinon(frc::Pose2d initial,frc::Pose2d desired) {
-    double distance=sqrt(pow(desired.x-initial.x,2)+pow(desired.y-initial.y,2))
+double MoveToPose::linearTranslation(frc::Pose2d current, frc::Pose2d desired) {
+    // double distance = sqrt(pow((double)desired.X() - current.X(), 2.0) + pow(0.0 + desired.Y() - current.Y(), 2.0));
+    double desiredx = desired.X().value();
+    double currentx = current.X().value();
+    double desiredy = desired.Y().value();
+    double currenty = current.Y().value();
+
+    
+    
+    double x = (desiredx - currentx) * (desiredx - currentx);
+    double y = (desiredy - currenty) * (desiredy - currenty);
+    
+    double distance = sqrt(x + y);
+    return distance;
 };
--- src/main/include/MoveToPose.h
+++ src/main/include/MoveToPose.h
@@ -1,9 +1,16 @@
-// TODO: what do all header files NEED to start with?
+#pragma once
 
 #include <frc/geometry/Pose2d.h>
 #include <cmath>
 #include <frc/trajectory/TrapezoidProfile.h>
 #include <frc/Timer.h>
+
+#include <units/voltage.h>
+#include <units/angle.h>
+//#include <units/acceleration.h>
+#include <units/angular_acceleration.h>
+#include <units/angular_velocity.h>
+//#include <units/math.h>
 
 
 class MoveToPose
@@ -12,28 +19,26 @@ class MoveToPose
         frc::TrapezoidProfile<units::degrees> m_Profile{
             frc::TrapezoidProfile<units::degrees>::Constraints{180_deg_per_s, 90_deg_per_s_sq}   
         };
-
+        int m_MoveAngleToState = 0;
         int m_MoveToState = 0;
-        frc::Timer m_timer; 
-    
+        frc::Timer m_timer;
+
+        // Moves robot from initial to desired
+        void move(frc::Pose2d current,frc::Pose2d desired);
+        
+        // Trapezoid move to handle angular rotation to desired pose
+        void angularRotation(frc::Rotation2d current, frc::Rotation2d desired);
+        
+        // Trapezoid move to handle linear translation to desired pose
+        double linearTranslation(frc::Pose2d current,frc::Pose2d desired);
+
     public:
         MoveToPose() = default;
         ~MoveToPose() = default;
 
         // Uses swerve odometry to generate the 'initial' pose
         void move(frc::Pose2d desired);
-
-        // TODO: all functions below should be private
-
-        // Moves robot from initial to desired
-        void move(frc::Pose2d initial,frc::Pose2d desired);
         
-        // Trapezoid move to handle angular rotation to desired pose
-        double angularRotation(frc::Pose2d desired, frc::Pose2d current);
-        
-        // Trapezoid move to handle linear translation to desired pose
-        frc::Pose2d linearTranslation(frc::Pose2d initial,frc::Pose2d desired);
-
         // TODO: create helper functions as needed
          
 };
