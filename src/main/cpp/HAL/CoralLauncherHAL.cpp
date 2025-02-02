0f367bcf6b38f3af91b6c7f303c0687cd90f6b6e
Student <ratpack-student@gmail.com>
2025-02-02 17:08:44 -0500

    actually finished corallauncherha l(added L1Launch and L2Launch)

--- src/main/cpp/HAL/CoralLauncherHAL.cpp
+++ src/main/cpp/HAL/CoralLauncherHAL.cpp
@@ -1,4 +1,137 @@
 #include "HAL/CoralLauncherHAL.h"
+/**/
+void CoralLauncher::L1Launch()
+{
+    switch(m_l1LaunchState)
+    {
+        case 0:
+        {
+            m_Timer.Stop();
+            m_Timer.Reset();
+            m_Timer.Start();
+
+            m_l1LaunchState++;
+            break;
+        }
+
+        case 1:
+        {
+            if(!m_beam_break.Get())
+            {
+                m_l1LaunchState++;
+                break;
+            }
+            SetIndexerSpeed(1.0);
+        }
+
+        case 2:
+        {
+            SetIndexerSpeed(0);
+            m_l1LaunchState++;
+            break;
+        }
+        
+        case 3:
+        {
+            if(m_beam_break.Get()) 
+            {
+                m_l1LaunchState++;
+                break;
+            }
+            SetRightWheelSpeed(-m_l1LaunchSpeed);
+            SetLeftWheelSpeed(m_l1LaunchSpeed);
+        }
+
+        case 4:
+        {
+            SetRightWheelSpeed(0);
+            SetLeftWheelSpeed(0);
+            break;
+        }
+    }
+}
+void CoralLauncher::L2Launch()
+{
+    switch(m_l2LaunchState)
+    {
+        case 0:
+        {
+            m_Timer.Stop();
+            m_Timer.Reset();
+            m_Timer.Start();
+
+            m_l2LaunchState++;
+            break;
+        }
+
+        case 1:
+        {
+            if(!m_beam_break.Get())
+            {
+                m_l2LaunchState++;
+                break;
+            }
+            SetIndexerSpeed(1.0);
+        }
+
+        case 2:
+        {
+            SetIndexerSpeed(0);
+            m_l2LaunchState++;
+            break;
+        }
+
+        case 3:
+        {
+            if(m_beam_break.Get())
+            {
+                m_l2LaunchState++;
+                break;
+            }
+            SetRightWheelSpeed(m_l2LaunchSpeed);
+            SetLeftWheelSpeed(m_l2LaunchSpeed);
+        }
+
+        case 4:
+        {
+            SetRightWheelSpeed(0);
+            SetLeftWheelSpeed(0);
+            break;
+        }
+    }
+}
+
+//void CoralLauncher::IndexerLaunch()
+// {
+//     switch(m_indexerState)
+//     {
+//         case 0:
+//         {
+//             m_Timer.Stop();
+//             m_Timer.Reset();
+//             m_Timer.Start();
+
+//             m_indexerState++;
+//             break;
+//         }
+
+//         case 1:
+//         {
+//             if(!m_beam_break.Get())
+//             {
+//                 m_indexerState++;
+//                 break;
+//             }
+//             SetIndexerSpeed(1.0);
+//         }
+
+//         case 2:
+//         {
+//             SetIndexerSpeed(0);
+//             break;
+//         }
+//     }
+// }
 
 void CoralLauncher::SetRightWheelSpeed(double speed)
 {
@@ -26,4 +159,14 @@ double CoralLauncher::GetRightWheelSpeed()
 double CoralLauncher::GetLeftWheelSpeed()
 {
     return m_LeftWheelSpeed;
+}
+
+void CoralLauncher::ResetL1LaunchState()
+{
+    m_l1LaunchState = 0;
+}
+
+void CoralLauncher::ResetL2LaunchState()
+{
+    m_l2LaunchState = 0;
 }
