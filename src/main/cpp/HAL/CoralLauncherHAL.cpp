cd7c682008f55d4f378cfbc2bd5180bddfec19d3
Student <ratpack-student@gmail.com>
2025-02-09 16:12:26 -0500

    Made MechanicsmConfig and variables - Alex

--- src/main/cpp/HAL/CoralLauncherHAL.cpp
+++ src/main/cpp/HAL/CoralLauncherHAL.cpp
@@ -1,6 +1,7 @@
 #include "HAL/CoralLauncherHAL.h"
 #include <rev/config/SparkMaxConfig.h>
 #include "ratpack/SparkMaxDebugMacro.h"
+#include "MechanismConfig.h"
 
 CoralLauncher::CoralLauncher()
 {
@@ -9,22 +10,23 @@ CoralLauncher::CoralLauncher()
     rev::spark::SparkMaxConfig indexer_config{};
 
     flywheel_config.closedLoop.SetFeedbackSensor(rev::spark::ClosedLoopConfig::FeedbackSensor::kPrimaryEncoder);
-    flywheel_config.closedLoop.Pidf(0,0,0,0);
-    flywheel_config.encoder.VelocityConversionFactor(0);
-    flywheel_config.Inverted(false);
-    flywheel_config.SetIdleMode(rev::spark::SparkMaxConfig::IdleMode::kCoast);
-    flywheel_config.SmartCurrentLimit(0,0,0); 
-    flywheel_config.VoltageCompensation(0); 
+    flywheel_config.closedLoop.Pidf(ratbot::CoralLauncherConfig::Flywheel::P,ratbot::CoralLauncherConfig::Flywheel::I,ratbot::CoralLauncherConfig::Flywheel::D,ratbot::CoralLauncherConfig::Flywheel::F);
+    flywheel_config.encoder.VelocityConversionFactor(ratbot::CoralLauncherConfig::Flywheel::VEL_CONV_FACTOR);
+    flywheel_config.Inverted(ratbot::CoralLauncherConfig::Flywheel::INVERTED);
+    flywheel_config.SetIdleMode(ratbot::CoralLauncherConfig::Flywheel::IDLE_MODE);
+    flywheel_config.SmartCurrentLimit(ratbot::CoralLauncherConfig::Flywheel::CURRENT_LIM); 
+    flywheel_config.VoltageCompensation(ratbot::VOLTAGE_COMPENSATION); 
 
     indexer_config.closedLoop.SetFeedbackSensor(rev::spark::ClosedLoopConfig::FeedbackSensor::kPrimaryEncoder);
-    indexer_config.Inverted(false);
-    indexer_config.SetIdleMode(rev::spark::SparkMaxConfig::IdleMode::kCoast);
-    indexer_config.SmartCurrentLimit(0,0,0);
-    indexer_config.VoltageCompensation(0);
+    indexer_config.Inverted(ratbot::CoralLauncherConfig::Indexer::INVERTED);
+    indexer_config.SetIdleMode(ratbot::CoralLauncherConfig::Indexer::IDLE_MODE);
+    indexer_config.SmartCurrentLimit(ratbot::CoralLauncherConfig::Indexer::CURRENT_LIM);
+    indexer_config.VoltageCompensation(ratbot::VOLTAGE_COMPENSATION);
 
     START_RETRYING(RIGHT_FLYWHEEL_CONFIG)
     m_rightMotor.Configure(flywheel_config, rev::spark::SparkMax::ResetMode::kResetSafeParameters, rev::spark::SparkMax::PersistMode::kPersistParameters);
     END_RETRYING
+    flywheel_config.Inverted(!ratbot::CoralLauncherConfig::Flywheel::INVERTED);
     START_RETRYING(LEFT_FLYWHEEL_CONFIG)
     m_leftMotor.Configure(flywheel_config, rev::spark::SparkMax::ResetMode::kResetSafeParameters, rev::spark::SparkMax::PersistMode::kPersistParameters);
     END_RETRYING
