14c00727baa74d341ee61a764e6d4543a30db065
Student <ratpack-student@gmail.com>
2025-02-09 15:51:19 -0500

    Skeleton of DeepCage

---
+++ src/main/cpp/HAL/DeepCageHAL.cpp
@@ -0,0 +1,29 @@
+#include "ratpack/SparkMaxDebugMacro.h"
+
+NeoTurnMotor :: SwerveTurnMotor {
+}
+
+     NeoTurnMotor() = default;
+{
+      
+    default = SetPosition
+}
+         void Configure(SwerveTurnMotorConfig &config) override;
+{
+
+
+}
+         void SetRotation(frc::Rotation2d deg) override; 
+{
+
+
+}
+         frc::Rotation2d GetRotation() override; 
+{
+
+
+}
+
+
+
+
