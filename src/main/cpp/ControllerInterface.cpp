f818860cf2c1a8c11f722a00e00722b797a6ffc3
Student <ratpack-student@gmail.com>
2025-02-28 18:52:41 -0500

    Lucas - added in controller interface for the smart planner (copilot). Also has some mystery algae stuff idk who did that

--- src/main/cpp/ControllerInterface.cpp
+++ src/main/cpp/ControllerInterface.cpp
@@ -35,3 +35,19 @@ void ControllerInterface::UpdateLauncherInput(RobotControlData &controlData){
     controlData.coralInput.setFlywheelToL1Speed = m_pilot.GetAButton();
     controlData.coralInput.setFlywheelToL2Speed = m_pilot.GetBButton();
 }
+
+void ControllerInterface::UpdateSmartplannerInput(RobotControlData &controlData)
+{
+    if (m_copilot.GetLeftTriggerAxis() > 0.1) {controlData.plannerInput.Left_L1 = true;}
+    else if (m_copilot.GetRightTriggerAxis() > 0.1) {controlData.plannerInput.Right_L1 = true;}
+    else if (m_copilot.GetLeftBumperButtonPressed()) {controlData.plannerInput.Left_L2 = true;}
+    else if (m_copilot.GetRightBumperButtonPressed()) {controlData.plannerInput.Right_L2 = true;}
+    else 
+    {
+        controlData.plannerInput.Left_L1 = false;
+        controlData.plannerInput.Left_L2 = false;
+        controlData.plannerInput.Right_L1 = false;
+        controlData.plannerInput.Right_L2 = false;
+    }
+
+}
