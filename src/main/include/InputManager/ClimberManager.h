e52597e98f80eca5f13a08788d27ba4baac3d383
Nick McGill <13747745+nick-mcgill@users.noreply.github.com>
2025-03-07 19:38:06 -0500

    [nick] Fix merge conflict

--- src/main/include/InputManager/ClimberManager.h
+++ src/main/include/InputManager/ClimberManager.h
@@ -3,13 +3,12 @@
 #include "HAL/ClimberHAL.h"
 #include "RobotControlData.h"
 #include <frc/Timer.h>
-// #include <frc/DigitalInput.h>
 
 
 class ClimberManager
 {
 public:
-    ClimberManager();
+    ClimberManager() = default;
     ~ClimberManager() = default;
     void ResetState();
     void HandleInput(RobotControlData &robotControlData);
