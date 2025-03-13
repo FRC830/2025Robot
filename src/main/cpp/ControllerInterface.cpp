0e80faa81b752ec4ccbec9630c88b430aae6d9e0
Student <ratpack-student@gmail.com>
2025-03-12 20:46:12 -0400

    some quick controllerinterface changes for algae arm (not done)

--- src/main/cpp/ControllerInterface.cpp
+++ src/main/cpp/ControllerInterface.cpp
@@ -64,6 +64,13 @@ void ControllerInterface::UpdateSwerveInput(RobotControlData &controlData)
     m_prevRightFeederButtonValue = tempTargetRightFeeder;
 }
 
+void ControllerInterface::UpdateAlgaeArmInput(RobotControlData &controlData)
+{
+    if (m_copilot.GetLeftBumperButtonPressed()) {controlData.algaeInput.RunRemoverBottom = true; }
+    else if (m_copilot.GetRightBumperButtonPressed()) {controlData.algaeInput.RunRemoverTop = true;}
+    else if (m_copilot.GetStartButtonPressed()) {controlData.algaeInput.RunRemoverStow = true;}
+}
+
 void ControllerInterface::UpdateLauncherInput(RobotControlData &controlData){
     controlData.coralInput.setFlywheelToL1Speed = m_copilot.GetAButton();
     controlData.coralInput.setFlywheelToL2Speed = m_copilot.GetBButton();
@@ -87,10 +94,10 @@ void ControllerInterface::UpdateLauncherInput(RobotControlData &controlData){
 
 void ControllerInterface::UpdateSmartplannerInput(RobotControlData &controlData)
 {
-    if (m_copilot.GetLeftTriggerAxis() > 0.1) {controlData.plannerInput.Left_L1 = true;}
-    else if (m_copilot.GetRightTriggerAxis() > 0.1) {controlData.plannerInput.Right_L1 = true;}
-    else if (m_copilot.GetLeftBumper()) {controlData.plannerInput.Left_L2 = true;}
-    else if (m_copilot.GetRightBumper()) {controlData.plannerInput.Right_L2 = true;}
+   // if (m_copilot.GetLeftTriggerAxis() > 0.1) {controlData.plannerInput.Left_L1 = true;}
+   // else if (m_copilot.GetRightTriggerAxis() > 0.1) {controlData.plannerInput.Right_L1 = true;}
+    if (m_copilot.GetLeftTriggerAxis() > 0.1) {controlData.plannerInput.Left_L2 = true;}
+    else if (m_copilot.GetRightTriggerAxis() > 0.1) {controlData.plannerInput.Right_L2 = true;}
     else 
     {
         controlData.plannerInput.Left_L1 = false;
