b8a31c1c6d43f0aa43ffa32bee95837006071c1a
Student <ratpack-student@gmail.com>
2025-09-28 14:47:38 -0400

    completely empty robot template

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -11,21 +11,6 @@
 #include <pathplanner/lib/commands/PathPlannerAuto.h>
 #include <frc/Timer.h>
 
-#include "ratpack/swerve/AnalogAbsoluteEncoder.h"
-#include "ratpack/swerve/NavXGyro.h"
-#include "ratpack/swerve/NeoDriveMotor.h"
-#include "ratpack/swerve/NeoTurnMotor.h"
-#include "ratpack/swerve/WPISwerveModule.h"
-#include "ratpack/swerve/WPISwerveDrive.h"
-
-#include "PhotonVisionCamera.h"
-#include "ControllerInterface.h"
-#include "RobotControlData.h"
-#include "MoveToPose.h"
-#include "InputManager/ClimberManager.h"
-#include "InputManager/CoralLauncherManager.h"
-#include "InputManager/AlgaeRemoverManager.h"
-#include "SmartPlanner.h"
 
 class Robot : public frc::TimedRobot {
  public:
@@ -47,32 +32,5 @@ class Robot : public frc::TimedRobot {
   void PrintSwerveInfo();
 
  private:
-  double GetSwerveDeadZone();
-
-  static const int NUM_MODULES = 4;
-
-  std::array<AnalogAbsoluteEncoder, NUM_MODULES> _abs_encoders;
-  std::array<NeoTurnMotor, NUM_MODULES> _turn_motors;
-  std::array<NeoDriveMotor, NUM_MODULES> _drive_motors;
-  std::array<WPISwerveModule, NUM_MODULES> _modules;
-  WPISwerveDrive _swerve;
-
-  NavXGyro _gyro;
-  ControllerInterface _controller_interface;
-  RobotControlData _robot_control_data;
-  MoveToPose m_rotateToFeeder;
-  CoralLauncherManager m_coralLauncherManager;
-  AlgaeRemoverManager m_algaeRemoverManager;
-
-  frc::Timer autonTimer;
   
-  int m_state = 0;
-  frc2::Command* m_auto;
-
-  frc::SendableChooser<frc2::Command*> m_autoChooser;
-  
-  std::shared_ptr<PhotonVisionCamera> m_cam;
-  std::shared_ptr<SmartPlanner> m_smartPlanner;
-
-  ClimberManager m_ClimberManager;
-};
+  };
