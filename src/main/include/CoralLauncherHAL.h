e483c41a7fa39ec809455b64bcb477c74071d7d3
TheCrudeOilBro <111211541+TheCrudeOilBro@users.noreply.github.com>
2025-10-12 11:38:01 -0400

    Create CoralLauncherHAL.h

---
+++ src/main/include/CoralLauncherHAL.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CanConfig.h"
+#include "RobotControlData.h"
+#include "rev/SparkMax.h"
+#include <frc/Timer.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/DigitalInput.h>
+
+class CoralLauncher
+{
+    public:
+        CoralLauncher();
+        ~CoralLauncher() = default; 
+    private:
+                         
+};
