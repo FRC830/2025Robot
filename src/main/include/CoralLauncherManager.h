a4c1a9a0d28f2020feb7d1d8a994c33d47932bc4
TheCrudeOilBro <111211541+TheCrudeOilBro@users.noreply.github.com>
2025-10-12 11:38:22 -0400

    Create CoralLauncherManager.h

---
+++ src/main/include/CoralLauncherManager.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "HAL/CoralLauncherHAL.h"
+#include "RobotControlData.h"
+#include <frc/DigitalInput.h>
+
+
+class CoralLauncherManager
+{
+public:
+    CoralLauncherManager() = default;
+    ~CoralLauncherManager() = default;
+
+private:
+
+};
