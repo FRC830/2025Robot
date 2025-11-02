9e4e4768dcdc7480fcfed8813427dc0403a6b323
Student <ratpack-student@gmail.com>
2025-11-02 17:44:04 -0500

    calibration adjustments from CAD6

--- src/main/include/ratpack/swerve/SwerveConfig.h
+++ src/main/include/ratpack/swerve/SwerveConfig.h
@@ -12,7 +12,7 @@
 // --------------------Absolute Encoder-----------------------
 const int FL_ABS_ENC_PORT = 3;
 const bool FL_ABS_ENC_INVERTED = false;
-const frc::Rotation2d FL_ZERO_HEADING{units::degree_t{268.76f}};
+const frc::Rotation2d FL_ZERO_HEADING{units::degree_t{267.62f}};
 
 ctre::phoenix6::hardware::CANcoder fl_abs_enc{FL_ABS_ENC_PORT};
 
@@ -39,7 +39,7 @@ rev::spark::SparkClosedLoopController fl_drive_pid = fl_drive_mtr.GetClosedLoopC
 // --------------------Absolute Encoder-----------------------
 const int FR_ABS_ENC_PORT = 6;
 const bool FR_ABS_ENC_INVERTED = false;
-const frc::Rotation2d FR_ZERO_HEADING{units::degree_t{270.61f}};
+const frc::Rotation2d FR_ZERO_HEADING{units::degree_t{271.14f}};
 
 ctre::phoenix6::hardware::CANcoder fr_abs_enc{FR_ABS_ENC_PORT};
 
@@ -66,7 +66,7 @@ rev::spark::SparkClosedLoopController fr_drive_pid = fr_drive_mtr.GetClosedLoopC
 // --------------------Absolute Encoder-----------------------
 const int BL_ABS_ENC_PORT = 12;
 const bool BL_ABS_ENC_INVERTED = false;
-const frc::Rotation2d BL_ZERO_HEADING{units::degree_t{3.86f}};
+const frc::Rotation2d BL_ZERO_HEADING{units::degree_t{5.00f}};
 
 ctre::phoenix6::hardware::CANcoder bl_abs_enc{BL_ABS_ENC_PORT};
 
@@ -94,7 +94,7 @@ rev::spark::SparkClosedLoopController bl_drive_pid = bl_drive_mtr.GetClosedLoopC
 // --------------------Absolute Encoder-----------------------
 const int BR_ABS_ENC_PORT = 9;
 const bool BR_ABS_ENC_INVERTED = false;
-const frc::Rotation2d BR_ZERO_HEADING{units::degree_t{176.48f}};
+const frc::Rotation2d BR_ZERO_HEADING{units::degree_t{174.55f}};
 
 ctre::phoenix6::hardware::CANcoder br_abs_enc{BR_ABS_ENC_PORT};
 
